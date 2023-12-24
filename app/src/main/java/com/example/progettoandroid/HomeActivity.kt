7b3619617b0515023154528ca206b412543c9c76
CiccioRomeo <154534332+CiccioRomeo@users.noreply.github.com>
2023-12-24 20:36:03 +0100

    db finito mappe iniziate

--- app/src/main/java/com/example/progettoandroid/HomeActivity.kt
+++ app/src/main/java/com/example/progettoandroid/HomeActivity.kt
@@ -1,13 +1,46 @@
 package com.example.progettoandroid
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.health.connect.datatypes.ExerciseRoute.Location
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.tasks.Task
 
-class HomeActivity : AppCompatActivity() {
-
+class HomeActivity : AppCompatActivity(),OnMapReadyCallback {
+    private lateinit var  myMap : GoogleMap
+    private lateinit var fusedLocationProviderClient : FusedLocationProviderClient
+    private lateinit var lastLocation : Location
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home)
+        //getLastLocation()
+        val mapFragmet: SupportMapFragment = getSupportFragmentManager().findFragmentById(R.id.map) as SupportMapFragment
+        mapFragmet.getMapAsync(this)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+   /* fun getLastLocation(){
+         if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return
+        }
+        val task: Task<android.location.Location> = fusedLocationProviderClient.getLastLocation()
+
+    }*/
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        myMap = googleMap;
     }
 }
--- app/src/main/java/com/example/progettoandroid/User.kt
+++
@@ -1,3 +0,0 @@
-package com.example.progettoandroid
-
-data class User (val email: String, val password: String,val nome: String,val cognome: String, val sesso : String, val peso:Int)
