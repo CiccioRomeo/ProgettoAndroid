7946f184ecd2b8f3b0194234075b5d150e661896
CiccioRomeo <154534332+CiccioRomeo@users.noreply.github.com>
2024-06-12 13:11:54 +0200

    fine

---
+++ app/src/main/java/com/example/progettoandroid/Register2Activity.kt
@@ -0,0 +1,13 @@
+package com.example.progettoandroid
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Register2Activity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register2)
+    }
+}
